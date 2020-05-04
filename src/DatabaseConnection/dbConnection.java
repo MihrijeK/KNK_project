557ce7a8cf11d071a20e94e12edc8047adee1262
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-04 21:55:41 +0200

    Klasa statike per konektim.

--- src/DatabaseConnection/dbConnection.java
+++ src/DatabaseConnection/dbConnection.java
@@ -4,14 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class dbConnection {
-    public Connection connection;
-    public Connection getConnection() throws Exception {
+public class  dbConnection {
+    public static Connection connection;
+    public static Connection getConnection() throws Exception {
         String userName = "root";
         String password = "riseandshine";
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost/dbhotel?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UCT";
             connection = DriverManager.getConnection(url,userName,password);
         } catch (Exception e) {
