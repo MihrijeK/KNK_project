73f5b9240ec8ed03596a8bb0510db5ded751d297
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-04 22:46:47 +0200

    Krijimi i contollerit per AddNewRoomView

---
+++ src/sample/Controllers/AddNewRoomView.java
@@ -0,0 +1,20 @@
+package sample.Controllers;
+
+import DatabaseConnection.dbConnection;
+import Helpers.Rooms;
+import Helpers.Staff;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AddNewRoomView {}
