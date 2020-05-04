5cdde24dd118082e7ecffa6668e4df8060ca141a
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-04 22:38:29 +0200

    Krijimi i controllerit AdminDashboard

---
+++ src/sample/Controllers/AdminDashboard.java
@@ -0,0 +1,28 @@
+package sample.Controllers;
+
+import DatabaseConnection.dbConnection;
+import Helpers.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.lang.String;
+
+
+public class AdminDashboard{
+
+}
