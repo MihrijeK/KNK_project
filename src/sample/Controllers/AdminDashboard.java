b1db39a69053877c9deb1a7c7aca2c619cadab69
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-04 22:39:21 +0200

    Deklarimi i fieldave

--- src/sample/Controllers/AdminDashboard.java
+++ src/sample/Controllers/AdminDashboard.java
@@ -24,5 +24,43 @@ import java.lang.String;
 
 
 public class AdminDashboard{
+    private Connection connection = dbConnection.getConnection();
 
+    @FXML private Button overviewBtn;
+    @FXML private Button staffBtn;
+    @FXML private Button roomsBtn;
+    @FXML private Button paymentsBtn;
+    @FXML private Button reservationsBtn;
+    @FXML private Button menusBtn;
+    @FXML private Button settingsBtn;
+    @FXML private Button logoutBtn;
+    @FXML private AnchorPane overviewPane;
+    @FXML private AnchorPane staffPane;
+
+    @FXML private Button CreateMemberBtn;
+    @FXML private Button refreshTableView;
+
+    public AnchorPane roomsPane;
+    @FXML
+    private Button btnAddNewRoom;
+
+    @FXML private TableView <Staff> showStaffTable;
+    @FXML private TableColumn<Staff, Integer> col_id;
+    @FXML private TableColumn<Staff,String> col_fname;
+    @FXML private TableColumn<Staff,String> col_lname;
+    @FXML private TableColumn<Staff, Integer> col_prsNum;
+    @FXML private TableColumn<Staff,String> col_position;
+    @FXML private TableColumn<Staff, Date> col_bday;
+    @FXML private TableColumn<Staff,String> col_phone;
+    @FXML private TableColumn<Staff, Integer> col_salary;
+
+    @FXML private TableView <Rooms> roomsTableView;
+    @FXML private TableColumn<Rooms, Integer> roomNumber;
+    @FXML private TableColumn<Rooms, Integer> floorNumber;
+    @FXML private TableColumn<Rooms, Integer> capacity;
+    @FXML private TableColumn<Rooms, Integer> bedNumber;
+    @FXML private TableColumn<Rooms, String> roomType;
+    @FXML private TableColumn<Rooms, Double> price;
+    public AdminDashboard() throws Exception {
+    }
 }
