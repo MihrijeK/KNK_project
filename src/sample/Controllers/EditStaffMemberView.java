bf0c5cdd9fe6c387055b6a57746ff97df711ce05
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-04 22:58:39 +0200

    Updated

--- src/sample/Controllers/EditStaffMemberView.java
+++ src/sample/Controllers/EditStaffMemberView.java
@@ -45,3 +45,48 @@ public class EditStaffMemberView {
     }
 
     private final Stage stage;
+    public EditStaffMemberView(Staff staff) throws Exception {
+        this.staff = staff;
+        stage = new Stage();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Views/EditStaffMemberView.fxml"));
+            loader.setController(this);
+
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Edit staff member");
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void display()  {
+        first_name.setText(staff.getFirstName());
+        last_name.setText(staff.getLastName());
+        personal_number.setText(Integer.toString(staff.getPersonalNumber()));
+        phone_number.setText(staff.getPhoneNumber());
+        salary.setText(Double.toString(staff.getSalary()));
+        password.setText(staff.getPassword());
+        editStaffMemberBtn.setOnAction(e->{
+            try {
+                updateStaffMember(staff.getId(),first_name.getText(),last_name.getText(),Integer.parseInt(personal_number.getText()),phone_number.getText(),500,password.getText());
+                stage.close();
+                System.out.println("Updated");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+        cancleCreation.setOnAction(e->stage.close());
+        stage.showAndWait();
+    }
+
+    public void updateStaffMember(int id,String fname,String lname,int prs_num,String phnNumber,int salary,String passwordd) throws Exception {
+        String query = "UPDATE staff SET first_name = '"+fname+"' ,last_name = '"+lname+"',personal_number = "+prs_num+",position = 'TEST',birthdate = '2020-05-01'," +
+                "phone_number = '"+phnNumber+"',salary = "+salary+",passwordd = '"+passwordd+"' WHERE id = "+id+";";
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+    }
+}
