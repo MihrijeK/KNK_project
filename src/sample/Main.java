920463cf85477cb1a48752fca7bc2c8937c00802
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-04 23:00:46 +0200

    Updated

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,7 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Views/AdminDashboard.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
