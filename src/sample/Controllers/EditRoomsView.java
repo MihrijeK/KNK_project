4816387f00cc2b7234a9f78507d847e5ebd5f11c
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-04 22:56:58 +0200

    Updated

--- src/sample/Controllers/EditRoomsView.java
+++ src/sample/Controllers/EditRoomsView.java
@@ -29,3 +29,43 @@ public class EditRoomsView {
     @FXML private TextField Price;
     @FXML private Button updateRoom;
     @FXML private Button cancleButton;
+
+    public EditRoomsView(Rooms rooms) throws Exception {
+        this.rooms = rooms;
+        stage = new Stage();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Views/EditRoomsView.fxml"));
+            loader.setController(this);
+
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Edit staff member");
+            stage.setScene(scene);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void display() throws IOException, SQLException {
+        roomNumber.setText(Integer.toString(rooms.getRoom_number()));
+        floorNumber.setText(Integer.toString(rooms.getFloor_number()));
+        roomCapacity.setText(Integer.toString(rooms.getCapacity()));
+        bedNumber.setText(Integer.toString(rooms.getBed_number()));
+        roomType.setText(rooms.getRoom_type());
+        Price.setText(Double.toString(rooms.getPrice()));
+
+        updateRoom.setOnAction(e->{
+            try {
+                updateRoom(Integer.parseInt(roomNumber.getText()),Integer.parseInt(floorNumber.getText()),Integer.parseInt(roomCapacity.getText()),Integer.parseInt(bedNumber.getText()),roomType.getText(),
+                        Double.parseDouble(Price.getText()));
+                System.out.println("Updated");
+                stage.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        cancleButton.setOnAction(e->stage.close());
+        stage.showAndWait();
+    }
