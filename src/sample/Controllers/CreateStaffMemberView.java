9c667626eb23ee58175e2022f40378215aaef68d
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2020-05-04 22:52:08 +0200

    Krijimi controllerit CreateStaffMemberView

---
+++ src/sample/Controllers/CreateStaffMemberView.java
@@ -0,0 +1,21 @@
+package sample.Controllers;
+
+import DatabaseConnection.dbConnection;
+import Helpers.Staff;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateStaffMemberView {}
