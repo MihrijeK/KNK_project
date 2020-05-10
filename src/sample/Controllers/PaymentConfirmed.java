e94b9d510db46bf90fff3b376e3cbb309e155851
ylber-gashi <58399076+ylber-gashi@users.noreply.github.com>
2020-05-10 21:28:10 +0200

    Importimi i moduleve te nevojshme

--- src/sample/Controllers/PaymentConfirmed.java
+++ src/sample/Controllers/PaymentConfirmed.java
@@ -1,6 +1,15 @@
 package sample.Controllers;
 
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 public class PaymentConfirmed implements Initializable {
 
