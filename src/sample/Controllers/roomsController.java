e278dec06b76fc13e3da6851e6f068238b6d1386
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-10 01:46:47 +0200

    Fushat per lidhjen me fxml file-in

--- src/sample/Controllers/roomsController.java
+++ src/sample/Controllers/roomsController.java
@@ -1,6 +1,20 @@
 package Controllers;
 
 public class roomsController implements Initializable {
+      @FXML private DatePicker firstDatePickerField;
+      @FXML private DatePicker lastDatePickerField;
+      @FXML private Button findButtonId;
+      @FXML private TableView<Rooms> tableView;
+      @FXML private TableColumn<Rooms,Integer> roomNumberCol;
+      @FXML private TableColumn<Rooms,Integer> roomFloorCol;
+      @FXML private TableColumn<Rooms,Integer> capacityCol;
+      @FXML private TableColumn<Rooms,Integer> bedsCol;
+      @FXML private TableColumn<Rooms,String> roomTypeCol;
+      @FXML private TableColumn<Rooms,Integer> priceCol;
+      @FXML private TableColumn<Rooms,CheckBox> reservationCol;
+      @FXML private Button makeReservation;
+      
+      
       @Override
       public void initialize(URL url, ResourceBundle resourceBundle) {
 
