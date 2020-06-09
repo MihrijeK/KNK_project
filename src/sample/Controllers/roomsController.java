e301eec70a3ad3040ad1f340b2d047cd0ca72a6d
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-10 01:41:24 +0200

    Krijimi i kontrollerit roomsController

---
+++ src/sample/Controllers/roomsController.java
@@ -0,0 +1,8 @@
+package Controllers;
+
+public class roomsController implements Initializable {
+      @Override
+      public void initialize(URL url, ResourceBundle resourceBundle) {
+
+      }
+    }
