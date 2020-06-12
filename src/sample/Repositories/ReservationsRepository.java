c2e179e3b65da03e6041657c61e75737cbf7a48b
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-12 15:00:10 +0200

    Krijimi i klases ReservationsRepository

---
+++ src/sample/Repositories/ReservationsRepository.java
@@ -0,0 +1,15 @@
+package Repositories;
+
+
+import Connectivity.dbConnection;
+import Helpers.Person;
+import javafx.scene.control.Alert;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ReservationsRepository {
+
+}
