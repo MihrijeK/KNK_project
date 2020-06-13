1aaceb52e8d06a5b3157a048548fef95338d89ba
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-13 21:29:55 +0200

    Update ReservationsRepository.java

--- src/sample/Repositories/ReservationsRepository.java
+++ src/sample/Repositories/ReservationsRepository.java
@@ -24,7 +24,12 @@ public class ReservationsRepository {
             prep.setString(1,id);
             ResultSet rs=prep.executeQuery();
 
-            
+            if(rs.next()){
+                Person person= new Person(rs.getInt(1),rs.getString(2),rs.getString(3),
+                        rs.getLong(4),rs.getString(6),rs.getString(8),
+                        rs.getDate(5));
+                return person;
+            }else return null;
 
     }
   
