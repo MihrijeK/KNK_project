07480dcba87ca3abf9ac911475ec473e0068364c
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-17 21:50:04 +0200

    Variablat ndihmese qe perdoren

---
+++ src/sample/Controllers/ReservationController.java
@@ -0,0 +1,8 @@
+public class ReservationsController implements Initializable {
+    private LocalDate checkin_date;
+    private LocalDate checkout_date;
+    private ObservableList<Rooms> roomsToBook;
+    private double total=0;
+    private int guestId=0;
+    private long days;
+}
