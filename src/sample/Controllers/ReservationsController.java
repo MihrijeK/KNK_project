abf3f830b990e69be26eac2ed980edda0e6b8375
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-17 22:27:36 +0200

    Variablat qe lidhen me FXML file-in

--- src/sample/Controllers/ReservationsController.java
+++ src/sample/Controllers/ReservationsController.java
@@ -6,6 +6,15 @@ public class ReservationsController implements Initializable {
     private int guestId=0;
     private long days;
     
+    @FXML private ScrollPane roomsPane;
+    @FXML private VBox verticalBox;
+    @FXML private ListView<String> roomsList;
+    @FXML private ScrollPane bookedRoomsScroll;
+    @FXML private TextField idField;
+    @FXML private Label firstName;
+    @FXML private Label lastName;
+    @FXML private Label totalField;
+    
     ReservationsRepository reservationsRepository=new ReservationsRepository();
     ObservableList<String> roomTypes= FXCollections.observableArrayList();
 }
