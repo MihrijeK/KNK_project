307255d3dd69cc9d818e893117088b244989a6d4
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-17 22:22:53 +0200

    Update ReservationsController.java

--- src/sample/Controllers/ReservationsController.java
+++ src/sample/Controllers/ReservationsController.java
@@ -5,4 +5,7 @@ public class ReservationsController implements Initializable {
     private double total=0;
     private int guestId=0;
     private long days;
+    
+    ReservationsRepository reservationsRepository=new ReservationsRepository();
+    ObservableList<String> roomTypes= FXCollections.observableArrayList();
 }
