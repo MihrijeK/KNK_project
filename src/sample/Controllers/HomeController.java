ff38a704fb74bfdad44933a4c8bbf62be0d441ca
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-06-19 16:47:41 +0200

    Krijimi i HomeController

---
+++ src/sample/Controllers/HomeController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+
+
+    @FXML private ImageView building;
+    @FXML private Label sunhotel;
+    @FXML private Button login_button;
