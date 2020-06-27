a5783898150afb2042b2f4be9d60e69062b8e6b6
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-06-28 01:39:49 +0200

    Create PaymentsRepository.java

---
+++ src/sample/Repositories/PaymentsRepository.java
@@ -0,0 +1,14 @@
+package sample.Repositories;
+
+import sample.ConnectDB;
+import sample.Controllers.Payments;
+import sample.Models.Guests;
+import sample.Models.Payment;
+import sample.Models.View.PaymentModel;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
