05bc6e99990674627c275a1da7921c47f27337d1
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-06-28 01:40:13 +0200

    Update PaymentsRepository.java

--- src/sample/Repositories/PaymentsRepository.java
+++ src/sample/Repositories/PaymentsRepository.java
@@ -12,3 +12,24 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+public class PaymentsRepository {
+
+    private static PaymentModel parseFromRes(ResultSet res) throws Exception {
+        int payment_id = res.getInt("payment_id");
+        String firstname = res.getString("first_name");
+        String lastname = res.getString("last_name");
+        Date date = res.getDate("checkin_date");
+        double price = res.getDouble("price");
+        int is_payed = res.getInt("is_payed");
+
+
+        return new PaymentModel(payment_id, firstname, lastname, date, price, is_payed);
+    }
+
+
+
+
+
+    }
+}
