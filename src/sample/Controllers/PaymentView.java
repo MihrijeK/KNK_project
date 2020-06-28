635016e7e044468de244317d339a1d0e85405e49
ylber-gashi <58399076+ylber-gashi@users.noreply.github.com>
2020-06-28 22:22:06 +0200

    Query for reserved rooms linked to the method

--- src/sample/Controllers/PaymentView.java
+++ src/sample/Controllers/PaymentView.java
@@ -64,11 +64,7 @@ public class PaymentView implements Initializable {
             connection = dbConnection.getConnection();
 
             //Rezervimet qe i ka bere klienti ne fjale
-            ResultSet tabela = connection.createStatement().executeQuery("select dh.room_number, dh.room_type, dh.price from rooms dh \n" +
-                    "inner join reservations r on r.room_id=dh.room_number " +
-                    "inner join payments p on p.id = r.payment_id "+
-                    "where r.guest_id = "+user +" and p.is_payed = 0;");
-
+            ResultSet tabela = PaymentRepository.roomsBill(user);
 //            while(tabela.next()){
 //                oblist.add(new Rooms(tabela.getInt("room_number"),
 //                        tabela.getString("room_type"), tabela.getDouble("price")));
