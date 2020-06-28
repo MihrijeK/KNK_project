60bc6c894704c5134dc2136cb65402f82eb25db3
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-06-28 22:54:49 +0200

    Update Payments.java

--- src/sample/Controllers/Payments.java
+++ src/sample/Controllers/Payments.java
@@ -1,1 +1,16 @@
 package sample.Controllers;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import sample.Models.Payment;
+import sample.Models.View.TableViewContent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
