4a2363807f2286ffd069ef1601dae6ac236697c0
ylber-gashi <58399076+ylber-gashi@users.noreply.github.com>
2020-06-29 00:10:31 +0200

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Views/paymentView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Views/Home.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
