a764e2b97dc0c973a9d6f0162b198b47970c4cbe
rrustemh <58404371+rrustemh@users.noreply.github.com>
2020-06-28 22:02:08 +0200

    Importimi i librarive

--- src/sample/Controllers/insertGuestController.java
+++ src/sample/Controllers/insertGuestController.java
@@ -1,4 +1,14 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ResourceBundle;
+
 public class Controller implements Initializable {
+    
   
 
     @Override
