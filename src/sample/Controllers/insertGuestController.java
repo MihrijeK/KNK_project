6fe020f34c015224dcb2f801ed6f30b006a74f88
rrustemh <58404371+rrustemh@users.noreply.github.com>
2020-06-28 22:02:57 +0200

    Krijimi i variablave per fields

--- src/sample/Controllers/insertGuestController.java
+++ src/sample/Controllers/insertGuestController.java
@@ -8,7 +8,14 @@ import java.text.ParseException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    
+    @FXML private TextField firstName;
+    @FXML private TextField lastName;
+    @FXML private TextField personalNumber;
+    @FXML private TextField phoneNumber;
+    @FXML private DatePicker birthDate;
+    @FXML private RadioButton male;
+    @FXML private RadioButton female;
+    @FXML private ToggleGroup genderToggleGroup;    
   
 
     @Override
