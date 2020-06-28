fe76bc1b2dd6f6715f6f11edc1fd0054d954c94f
rrustemh <58404371+rrustemh@users.noreply.github.com>
2020-06-28 22:01:17 +0200

    Implementimi i interface Initializable

--- src/sample/Controllers/insertGuestController.java
+++ src/sample/Controllers/insertGuestController.java
@@ -1,1 +1,10 @@
+public class Controller implements Initializable {
+  
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        genderToggleGroup = new ToggleGroup();
+        this.male.setToggleGroup(this.genderToggleGroup);
+        this.female.setToggleGroup(this.genderToggleGroup);
+    }
+}
