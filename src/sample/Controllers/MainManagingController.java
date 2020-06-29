68b80647f25ce1e805884e5089d277d25e328c33
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-29 23:13:43 +0200

    Lidhja me elementet e fxml file-it

--- src/sample/Controllers/MainManagingController.java
+++ src/sample/Controllers/MainManagingController.java
@@ -1,5 +1,9 @@
 package sample.Controllers;
 
 public class MainManagingController implements Initializable {
-
+    @FXML private Button mainBtn;
+    @FXML private Button reservationsBtn;
+    @FXML private Button paymentsBtn;
+    @FXML private Button servicesBtn;
+    @FXML private Pane mainPane;
 }
