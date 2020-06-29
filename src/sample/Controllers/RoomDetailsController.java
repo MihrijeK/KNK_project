174d4cce1d5039016916df44e18f033bbf221bf1
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-29 20:12:16 +0200

    Vendosja e action event kur klikojme mbi remove

--- src/sample/Controllers/RoomDetailsController.java
+++ src/sample/Controllers/RoomDetailsController.java
@@ -10,4 +10,8 @@ public class RoomDetailsController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+    
+    public void setRemoveButtonAction(EventHandler<ActionEvent> eventHandler){
+        removeButton.setOnAction(eventHandler);
+    }
 }
