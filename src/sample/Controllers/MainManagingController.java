81634a94b45986d8deacbed0ab51a16715498fd1
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-29 23:14:22 +0200

    Implementimi i metodes initialize

--- src/sample/Controllers/MainManagingController.java
+++ src/sample/Controllers/MainManagingController.java
@@ -6,4 +6,13 @@ public class MainManagingController implements Initializable {
     @FXML private Button paymentsBtn;
     @FXML private Button servicesBtn;
     @FXML private Pane mainPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try{
+            viewLoader("Reservations");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
