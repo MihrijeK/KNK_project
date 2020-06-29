5fa9d6cfc37dd6b824744670a38675c79439dd77
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-29 12:30:22 +0200

    Loading room card view

--- src/sample/Components/RoomDetails.java
+++ src/sample/Components/RoomDetails.java
@@ -2,6 +2,9 @@ package sample.Components
 
 public class RoomDetails {
    public Node singleComponent(Rooms room, long daysToStay, EventHandler<ActionEvent> handler) throws Exception{
-   
+      FXMLLoader loader=new FXMLLoader();
+      URL url=new File("src/views/room-details.fxml").toURI().toURL();
+      loader.setLocation(url);
+      
    }
 }
