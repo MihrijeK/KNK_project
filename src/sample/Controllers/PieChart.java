ef11bd4960b48d8d93c068ac11be665d100795ba
MihrijeK <42340737+MihrijeK@users.noreply.github.com>
2020-06-29 10:09:13 +0200

    Importimi i librarive per nxjerrjen e statistikave

---
+++ src/sample/Controllers/PieChart.java
@@ -0,0 +1,11 @@
+package sample.Controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.event.EventHandler.*;
+import javafx.scene.text.FontWeight;
+import javafx.scene.input.MouseEvent;
