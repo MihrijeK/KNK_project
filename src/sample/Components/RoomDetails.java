841be13b27775ca6f6364c6ed11ed495298f03d3
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-29 12:09:32 +0200

    Metoda singleComponent per paraqitjen e detajeve

--- src/sample/Components/RoomDetails.java
+++ src/sample/Components/RoomDetails.java
@@ -1,5 +1,7 @@
 package sample.Components
 
 public class RoomDetails {
+   public Node singleComponent(Rooms room, long daysToStay, EventHandler<ActionEvent> handler) throws Exception{
    
+   }
 }
