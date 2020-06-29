b1446f266aa24167ea2a2cd6f35c6e03baafcc3c
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-29 20:09:37 +0200

    Heqja e comment

--- src/sample/Components/RoomDetails.java
+++ src/sample/Components/RoomDetails.java
@@ -1,15 +1,15 @@
-//package sample.Components;
-//
-//public class RoomDetails {
-//   public Node singleComponent(Rooms room, long daysToStay, EventHandler<ActionEvent> handler) throws Exception{
-//      FXMLLoader loader=new FXMLLoader();
-//      URL url=new File("src/views/room-details.fxml").toURI().toURL();
-//      loader.setLocation(url);
-//      Pane pane = loader.load();
-//        RoomDetailsController controller=loader.getController();
-//        controller.getRoomToShow(room,daysToStay);
-//        controller.setRemoveButtonAction(handler);
-//
-//        return pane;
-//   }
-//}
+package sample.Components;
+
+public class RoomDetails {
+  public Node singleComponent(Rooms room, long daysToStay, EventHandler<ActionEvent> handler) throws Exception{
+     FXMLLoader loader=new FXMLLoader();
+     URL url=new File("src/views/room-details.fxml").toURI().toURL();
+     loader.setLocation(url);
+     Pane pane = loader.load();
+       RoomDetailsController controller=loader.getController();
+       controller.getRoomToShow(room,daysToStay);
+       controller.setRemoveButtonAction(handler);
+
+       return pane;
+  }
+}
