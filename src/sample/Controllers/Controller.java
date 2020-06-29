7786908c953bb389dcd38c583d2854f83724b7e8
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-06-30 00:54:16 +0200

    Krijimi i Controller.java per login

---
+++ src/sample/Controllers/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import DatabaseConnection.dbConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import javax.swing.*;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
