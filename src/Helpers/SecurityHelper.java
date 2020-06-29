9187448b5991d44d49979f0547c7364edf028aaf
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-06-30 00:57:05 +0200

    Konstruktori i SecurityHelper

--- src/Helpers/SecurityHelper.java
+++ src/Helpers/SecurityHelper.java
@@ -3,3 +3,9 @@ package sample;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Base64;
+
+public class SecurityHelper {
+
+    public SecurityHelper(){
+
+    }
