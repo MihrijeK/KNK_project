5715f606eb5b9181ebc6c67dbbe301e09b649d3d
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-06-30 14:25:24 +0200

    Update PaymentModel.java

--- src/sample/Models/View/PaymentModel.java
+++ src/sample/Models/View/PaymentModel.java
@@ -2,5 +2,12 @@ package sample.Models.View;
 
 import java.util.Date;
 public class PaymentModel {
+        private int payment_id;
+        private String firstname;
+        private String lastname;
+        private Date date;
+        private double price;
+        private int isPayed;
+
 
 }
