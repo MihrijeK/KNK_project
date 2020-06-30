d973e49722e0b8c38f8cca10d4c5ad4390dfcbfe
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-30 18:38:28 +0200

    Konstruktori i klases

--- src/Helpers/GuestPayment.java
+++ src/Helpers/GuestPayment.java
@@ -6,3 +6,12 @@ public class GuestPayment {
     private String lastname;
     private String date;
     private double price;
+
+    public GuestPayment(int id,String firstname,String lastname,String date,double price){
+        this.id=id;
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.date=date;
+        this.price=price;
+    }
+}
