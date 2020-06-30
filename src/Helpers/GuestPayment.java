dce91b753b12db21380077c66ea6d9338527d5da
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-30 18:37:57 +0200

    Variablat e klases

--- src/Helpers/GuestPayment.java
+++ src/Helpers/GuestPayment.java
@@ -1,1 +1,8 @@
+package Helpers;
 
+public class GuestPayment {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String date;
+    private double price;
