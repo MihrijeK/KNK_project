58a7a471b71b9a665b7bdbb22211d0c5421779f0
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-30 18:33:07 +0200

    Metoda per shtimin e butonit pay ne tabele

--- src/sample/Controllers/Partials/AddButton.java
+++ src/sample/Controllers/Partials/AddButton.java
@@ -1,23 +1,34 @@
 package sample.Controllers.Partials;
 
+import Helpers.GuestPayment;
 import Helpers.Rooms;
 import Helpers.Service_Type;
 import Helpers.Staff;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import sample.Controllers.Admin.RoomControllers.EditRoomsView;
 import sample.Controllers.Admin.Services.EditServiceType;
 import sample.Controllers.Admin.StaffControllers.EditStaffMemberView;
+import sample.Controllers.PaymentView;
+import sample.Controllers.ReservationsController;
 import sample.Repositories.RoomRespository;
 import sample.Repositories.ServicesTypeRepository;
 import sample.Repositories.StaffRepository;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
 
 public class AddButton {
@@ -205,11 +216,7 @@ public class AddButton {
 
                     {
                         btn.setOnAction((ActionEvent event) -> {
-
                             Rooms data = getTableView().getItems().get(getIndex());
-                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                            alert.setTitle("Delte Room");
-                            alert.showAndWait();
                             try {
                                 RoomRespository.remove(data.getRoom_number());
                                 showRoomsTable.getItems().remove(getIndex());
@@ -276,6 +283,58 @@ public class AddButton {
         };
         colBtn.setCellFactory(cellFactory);
         serviceTable.getColumns().add(colBtn);
+    }
+
+    public static void addPayButton(TableView<GuestPayment> paymentTable, String btnName, AnchorPane anchor) {
+        TableColumn<GuestPayment, Void> colBtn = new TableColumn(btnName);
+
+        Callback<TableColumn<GuestPayment, Void>, TableCell<GuestPayment, Void>> cellFactory = new Callback<TableColumn<GuestPayment, Void>, TableCell<GuestPayment, Void>>() {
+            @Override
+            public TableCell<GuestPayment, Void> call(final TableColumn<GuestPayment, Void> param) {
+                final TableCell<GuestPayment, Void> cell = new TableCell<GuestPayment, Void>() {
+                    private final javafx.scene.control.Button btn = new javafx.scene.control.Button(btnName);
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+
+                            GuestPayment data = getTableView().getItems().get(getIndex());
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                            alert.setTitle("Pay");
+                            alert.showAndWait();
+                            try {
+                                GuestPayment guestPayment=paymentTable.getItems().get(getIndex());
+                                URL url = new File("src/sample/Views/PaymentView.fxml").toURI().toURL();
+                                FXMLLoader loader = new FXMLLoader();
+                                loader.setLocation(url);
+                                Pane newScreen = loader.load();
+                                PaymentView controller=loader.getController();
+                                controller.getPaymentId(guestPayment.getId());
+
+                                Scene scene=new Scene(newScreen);
+                                Stage stage=(Stage)anchor.getScene().getWindow();
+                                stage.setScene(scene);
+                                stage.show();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+        colBtn.setCellFactory(cellFactory);
+        paymentTable.getColumns().add(colBtn);
     }
 
 }
