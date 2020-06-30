87ea3c8ac0ed23f48fee02c81ba76eab0583800b
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-30 15:39:26 +0200

    Shtim i metodes getUser

--- src/sample/Controllers/MainManagingController.java
+++ src/sample/Controllers/MainManagingController.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 import java.io.File;
@@ -19,6 +20,7 @@ public class MainManagingController implements Initializable {
     @FXML private Button paymentsBtn;
     @FXML private Button servicesBtn;
     @FXML private Pane mainPane;
+    @FXML private Label loggedInUser;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -62,7 +64,7 @@ public class MainManagingController implements Initializable {
                 //dritarja main e mires
                 break;
             case "Reservations":
-                URL url = new File("src/views/rooms.fxml").toURI().toURL();
+                URL url = new File("src/sample/Views/rooms.fxml").toURI().toURL();
                 loader.setLocation(url);
                 node = loader.load();
                 break;
@@ -78,5 +80,9 @@ public class MainManagingController implements Initializable {
         }
         mainPane.getChildren().clear();
         mainPane.getChildren().add(node);
+    }
+
+    public void getUser(String emri,String mbiemri){
+        loggedInUser.setText("Logged In:"+emri+" "+mbiemri);
     }
 }
